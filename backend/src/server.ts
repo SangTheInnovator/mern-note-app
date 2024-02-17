be02fe60ca79ed67b0ffc318daa40c5b2be39da3
Nguyen Phu Sang Le <phusang2501@gmail.com>
2024-02-17 23:03:37 +0200

    MongoDB model setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,14 @@
-import "dotenv/config";
+import app from './app';
 import env from './util/validateEnv';
 import mongoose from "mongoose";
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req,res) => {
-    res.send('Hello World');
-})
 
 const port = env.PORT;
 
 mongoose.connect(env.MONGO_CONNECTION_STRING)
-.then(() => {
-    console.log("Mongoose connected");
-    app.listen(port, () => {
-        console.log('Server running on port: ' + port);
-    });
-})
-.catch(console.error)
+    .then(() => {
+        console.log("Mongoose connected");
+        app.listen(port, () => {
+            console.log('Server running on port: ' + port);
+        });
+    })
+    .catch(console.error);
